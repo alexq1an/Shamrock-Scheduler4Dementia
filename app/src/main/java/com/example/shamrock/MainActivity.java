57e89d1e87acffb45c0247e316242da6257b3f88
dparaiso <dparaiso@sfu.ca>
2022-07-04 13:15:52 -0700

    attempt to add and read to firestore cloud

--- app/src/main/java/com/example/shamrock/MainActivity.java
+++ app/src/main/java/com/example/shamrock/MainActivity.java
@@ -1,10 +1,55 @@
 package com.example.shamrock;
 
-import androidx.appcompat.app.AppCompatActivity;
+//import static android.content.ContentValues.TAG;
 
 import android.os.Bundle;
+//import android.util.Log;
+
+//import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+//import com.google.android.gms.tasks.OnCompleteListener;
+//import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+//import com.google.firebase.firestore.QueryDocumentSnapshot;
+//import com.google.firebase.firestore.QuerySnapshot;
+
+//import java.util.HashMap;
+//import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+    public static void main(String[] Args) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        // Create a new user with a first and last name
+//        Map<String, Object> user = new HashMap<>();
+//        user.put("Name", "Carol");
+//
+//        //creating a new collection
+//        db.collection("TempCollection")
+//                .add(user)
+//                .addOnSuccessListener(documentReference -> Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId()))
+//                .addOnFailureListener(e -> Log.w(TAG, "Error adding document", e));
+//
+//        db.collection("users")
+//                .get()
+//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//
+//                    }
+//
+//
+//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                        if (task.isSuccessful()) {
+//                            for (QueryDocumentSnapshot document : task.getResult()) {
+//                                Log.d(TAG, document.getId() + " => " + document.getData());
+//                            }
+//                        } else {
+//                            Log.w(TAG, "Error getting documents.", task.getException());
+//                        }
+//                    }
+//                });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
