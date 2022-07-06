d18f41c4627f53a50bb9976212ca724ed9524057
dparaiso <dparaiso@sfu.ca>
2022-07-06 13:57:23 -0700

    integrated the class caregiver, and can grab based on unique autogenerated id, we need to make login button and function based on this functionality

---
+++ app/src/main/java/com/example/shamrock/Caregiver.java
@@ -0,0 +1,49 @@
+package com.example.shamrock;
+
+import com.google.firebase.firestore.Exclude;
+
+import java.util.ArrayList;
+
+public class Caregiver {
+    private String documentId;
+    private String name;
+    private String username;
+    private String password;
+//    private String pList;
+
+    public Caregiver() {
+        //public no-arg constructor needed
+    }
+
+    @Exclude
+    public String getDocumentId() {
+        return documentId;
+    }
+
+    public void setDocumentId(String documentId) {
+        this.documentId = documentId;
+    }
+
+    public Caregiver(String name, String username, String password) {
+        this.name = name;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Caregiver(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
--- app/src/main/java/com/example/shamrock/MainActivity.java
+++ app/src/main/java/com/example/shamrock/MainActivity.java
@@ -2,68 +2,165 @@ package com.example.shamrock;
 
 //import static android.content.ContentValues.TAG;
 
-
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.Source;
+import com.google.firestore.v1.WriteResult;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
-    private static final String KEY_TITLE = "title";
-    private static final String KEY_DESCRIPTION = "description";
+    private static final String KEY_NAME = "Name";
+    private static final String KEY_TITLE = "Username";
+    private static final String KEY_DESCRIPTION = "password";
 
-    private EditText editTextTitle;
-    private EditText editTextDescription;
+    private EditText editTextName;
+    private EditText editTextUsername;
+    private EditText editTextPassword;
+    private TextView textViewData;
 
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private DocumentReference dRef = db.collection("Caregiver").document("My First Note");
+    private CollectionReference cRef = db.collection("Caregiver");
 
+    private ArrayList<Caregiver> caregiverList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
-        editTextTitle = findViewById(R.id.edit_text_title);
-        editTextDescription = findViewById(R.id.edit_text_description);
+//        editTextName = findViewById(R.id.edit_text_name);
+        editTextUsername = findViewById(R.id.edit_text_username);
+        editTextPassword = findViewById(R.id.edit_text_password);
+        textViewData = findViewById(R.id.text_view_data);
     }
 
-    public void saveNote(View v){
-        String title = editTextTitle.getText().toString();
-        String description = editTextDescription.getText().toString();
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        cRef.addSnapshotListener(this, new EventListener<QuerySnapshot>() {
+//            @Override
+//            public void onEvent(QuerySnapshot queryDocumentSnapshots, FirebaseFirestoreException e) {
+//                if (e != null){
+//                    return;
+//                }
+//
+//                String data = "";
+//                for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots){
+//                    Caregiver caregiver = documentSnapshot.toObject(Caregiver.class);
+//                    caregiver.setDocumentId(documentSnapshot.getId());
+//
+//                    String documentId = caregiver.getDocumentId();
+//                    String name = caregiver.getName();
+//                    String username = caregiver.getUsername();;
+//                    String password = caregiver.getPassword();
+//
+//                    data += "ID: " + documentId + "\nName: " + name +
+//                            "\nUsername: " + username + "\nPassword: " + password + "\n\n";
+//                }
+//                textViewData.setText(data);
+//            }
+//        });
+//    }
 
-        Map<String, Object> note = new HashMap<>();
-        note.put(KEY_TITLE, title);
-        note.put(KEY_DESCRIPTION, description);
+    public void createAccount(View v){
+//        String name = editTextName.getText().toString();
+        String user = editTextUsername.getText().toString();
+        String password = editTextPassword.getText().toString();
 
-//        db.document("Notebook/My First Note");
-        db.collection("Notebook").document("My First Note").set(note)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
+//        Caregiver caregiver = new Caregiver(name, user, password);
+        Caregiver caregiver = new Caregiver(user, password);
+
+        DocumentReference addedDocRef = cRef.document();
+        addedDocRef.set(caregiver);
+        caregiver.setDocumentId(addedDocRef.getId());
+        caregiverList.add(caregiver);
+
+                //not sure if these works
+//                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+//                    @Override
+//                    public void onSuccess(DocumentReference documentReference) {
+//                        Toast.makeText(MainActivity.this, "Account created", Toast.LENGTH_SHORT).show();
+//                    }
+//
+//                })
+//                .addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        Toast.makeText(MainActivity.this, "Error!", Toast.LENGTH_SHORT).show();
+//                        Log.d(TAG, e.toString());
+//                    }
+//                });
+
+
+
+    }
+
+    public void updatePassword(View v){
+        String password = editTextPassword.getText().toString();
+        String user = editTextUsername.getText().toString();
+        int i = 0;
+        for( i = 0; i < caregiverList.size(); i++){
+            if(caregiverList.get(i).getUsername().equals(user)){
+                break;
+            }
+        }
+
+        if(i < caregiverList.size()){
+            cRef.document(caregiverList.get(i).getDocumentId()).update(KEY_DESCRIPTION, password);
+        }
+
+    }
+
+    public void loadNote(View v){
+        cRef.get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
                     @Override
-                    public void onSuccess(Void unused) {
-                        Toast.makeText(MainActivity.this, "Note Saved", Toast.LENGTH_SHORT).show();
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(MainActivity.this, "Error!", Toast.LENGTH_SHORT).show();
-                        Log.d(TAG, e.toString());
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        String data = "";
+
+                        for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots){
+                            Caregiver caregiver = documentSnapshot.toObject(Caregiver.class);
+                            caregiver.setDocumentId(documentSnapshot.getId());
+
+                            String documentId = caregiver.getDocumentId();
+//                            String name = caregiver.getName();
+                            String username = caregiver.getUsername();;
+                            String password = caregiver.getPassword();
+                            //add name
+                            data += "ID: " + documentId + "\nName: " +
+                                    "\nUsername: " + username + "\nPassword: " + password + "\n\n";
+
+//
+                        }
+                        textViewData.setText(data);
                     }
                 });
     }
-
-
 }
